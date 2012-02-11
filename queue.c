15de05ce0616ed2dcacf58bd3ba27289ce9288e0
Dan Mane <danmane@gmail.com>
2012-02-11 00:49:14 -0600

    Added queue funcitonality; about to incorporate with binaryTree.c to enable serialization

---
+++ queue.c
@@ -0,0 +1,77 @@
+#include <stdlib.h>
+#include <assert.h>
+#include <stdio.h>
+
+#include "queue.h"
+
+/*
+typedef struct queue *queue, queue_store;
+
+queue create_queue();
+void* queue_pop(queue);
+void* queue_peek(queue);
+int queue_append(void*);
+int queue_size(queue);
+*/
+
+typedef struct queue_cell{
+    void *contents;
+    struct queue_cell *next;
+} *queue_cell, queue_cell_store;
+
+struct queue{
+    queue_cell front;
+    queue_cell rear;
+    int size;
+};
+
+queue create_queue(){
+    queue newQueue=(queue) malloc(sizeof(queue_store));
+    newQueue->front=NULL;
+    newQueue->rear=NULL;
+    int size=0;
+};
+
+void *queue_pop(queue input){
+    queue_cell cell=input->front;
+    if (cell==NULL){
+        printf("Cell==NULL\n");
+        return NULL;
+    }
+    void *temp=cell->contents;
+    if (input->size > 1){
+        void* next=cell->next;
+        input->front=next;
+    }
+    else{
+        input->front=NULL;
+        input->rear=NULL;
+    }
+    input->size--;
+    free(cell);
+    return temp;
+}
+
+void* queue_peek(queue input){
+    if (input->front==NULL)
+        return NULL;
+    return input->front->contents;
+}
+
+int queue_append(queue input, void *contents){
+    queue_cell newCell = (queue_cell) malloc(sizeof(queue_cell_store));
+    newCell->contents=contents;
+    newCell->next=NULL;
+    if (input->size>0)
+        input->rear->next=newCell;
+    else
+        input->front=newCell;
+    input->rear=newCell;
+    input->size++;
+    return 0;
+    }
+    
+int queue_size(queue input){
+    return input->size;
+}
+
---
+++ queue.h
@@ -0,0 +1,12 @@
+#ifndef QUEUE_H
+#define QUEUE_H
+
+typedef struct queue *queue, queue_store;
+
+queue create_queue();
+void* queue_pop(queue);
+void* queue_peek(queue);
+int queue_append(queue, void*);
+int queue_size(queue);
+
+#endif
